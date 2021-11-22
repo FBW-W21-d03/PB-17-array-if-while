e0b5094f58d0c767fbf110a4ffbff3c7b0758903
sprotte <metatron1972@live.de>
2021-11-22 14:22:07 +0100

    sprotte

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ console.log("grade");
 
 
 while ((grade.length<=100) && (ungrade.length<=100)) {
-
     grade.push(ausgabe);
     ungrade.push(ausgabe+1);
     ausgabe+=2;//ausgabe=ausgabe+2
-
-    
 };
 console.log(grade,ungrade);
