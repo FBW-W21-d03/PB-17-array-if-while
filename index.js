eaa1cf90ffb1609ce0129e286d6245ed52cf6f49
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-22 13:35:06 +0100

    Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let i= 0;
+while( i <= 100){
+    i++;
+    if(i%2 == 0){
+        console.log(i + ",")
+
+    }
+    
+}
+
+let x= 0;
+while( x <= 100){
+    x++;
+    if(x%2 != 0){
+        console.log(x + ",")
+
+    }
+    
+}
