8e03af51d79c828f0a0d47a90f9aa84b4dac6e11
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-22 15:29:11 +0100

    Lösung von Christian

--- neu.js
+++ neu.js
@@ -1,19 +1,25 @@
-var geradeZahlen = [];
-const ungeradeZahlen = [];
+let geradeZahlen = []; // leeres Array
+let ungeradeZahlen = []; // leeres Array
 let aktuelleZahl = 2; // geht das auch mit const? Nein!
 
+// "||" das Zeichen für ODER. Wir benutzen ODER damit *beide* Arrays
+// am Ende 100 Elemente haben.
+// Auf Tastatur: "ALT GR" + "<"
 while (geradeZahlen.length < 100 || ungeradeZahlen.length < 100) {
   if (aktuelleZahl % 2 == 0) {
     // % - (ganzzahliger) Rest einer Division
-    // 3 % 2 = 1
-    // 4 % 2 = 0
-    // 5 % 2 = 1
-    // 6 % 2 = 0
+    // 2 % 2 = 0 -> gerade / even
+    // 3 % 2 = 1 -> ungerade / odd
+    // 4 % 2 = 0 -> gerade / even
+    // 5 % 2 = 1 -> ungerade / odd
+    // 6 % 2 = 1 -> gerade / even
     geradeZahlen.push(aktuelleZahl);
+    // fügt aktuelleZahl dem Array geradeZahlen hinzu
   } else {
+    // Andernfalls fügt aktuelleZahl dem Array ungeradeZahlen hinzu
     ungeradeZahlen.push(aktuelleZahl);
   }
-  aktuelleZahl++;
+  aktuelleZahl++; // entspricht aktuelleZahl = aktuelleZahl + 1;
 }
 
 console.log("Array von geraden Zahlen:", geradeZahlen);
