729fa56a067e3fa95dcf9994580893a5ccbce7e7
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-22 13:53:13 +0100

    ???????????

---
+++ PB-17-array-if-while-enza.js
@@ -0,0 +1,23 @@
+
+let i= 0;
+let array1 = [i];
+
+while( i <= 100){
+    i++;
+    if(i%2 == 0){
+        console.log([i]);
+    }    
+}
+
+
+let x= 0;
+let array2 = [x];
+
+while( x <= 100){
+    x++;
+    if(x%2 != 0){
+        console.log([x]);
+
+    }
+    
+}
