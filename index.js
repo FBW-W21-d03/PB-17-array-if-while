3a70efc59c432a98222896b77c158c4351cfd28b
kawanaza <kawanbabani@gmail.com>
2021-11-22 14:26:38 +0100

    Done

---
+++ index.js
@@ -0,0 +1,19 @@
+ let number =0 ;
+let leer =["    "];
+while (number <=100) {
+               number++;
+               if(number%2==0)
+                  console.log(number);      
+             
+  
+} 
+
+let numberTotop = 0;
+let lEer =[ ];
+while (numberTotop <= 100) {
+               numberTotop++;
+            if(numberTotop %2 !=0 )
+                console.log(numberTotop,[]);      
+             
+  
+} 
