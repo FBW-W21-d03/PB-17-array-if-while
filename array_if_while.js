9b2e8f6e9128baa37c36fb32025283a38777e89d
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-22 13:53:22 +0100

    Orhan#S lösung

--- array_if_while.js
+++ array_if_while.js
@@ -2,18 +2,16 @@ let arr1 = [];
 let arr2 = [];
 
 
-while (arr1 <= 100) {
+while (arr1 <= 200) {
     arr1++;
-
     if (arr1 %2 == 0) {
-       console.log(arr1)
+       console.log(arr1++)
     }    
 }
 
 
-while (arr2 <= 100) {
+while (arr2 <= 200) {
     arr2++;
-    
     if (arr2 %3 == 0) {
        console.log(arr2)
     }  
