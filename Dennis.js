f41d71a87d49e4341e6bd2abf0ee074033fa2611
Dennisbullys <d.rochus@hotmail.de>
2021-11-22 13:59:25 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,27 @@
+console.log("Aufgabe 1");
+
+ // Aufgabe 1
+
+let spotsRemaining =  200
+while (spotsRemaining > - 2){
+    console.log(spotsRemaining)
+    spotsRemaining=spotsRemaining -2;
+}
+
+
+// Aufgabe 2
+
+let spotsRemaining2 = 201;
+while (spotsRemaining < 3 ) {
+  console.log(spotsRemaining2);
+  spotsRemaining2 = spotsRemaining2 = -3;
+}
+
+
+
+
+
+
+
+
+
---
+++ dennis2.js
@@ -0,0 +1,27 @@
+console.log("Aufage 1")
+
+const grade=[]
+
+const ungrade=[]
+
+let number=2
+
+while ((grade.length<=100) && (ungrade.length<=100))
+{
+  grade.push(number);
+  ungrade.push(number+1);
+  number+=2;
+};
+console.log(grade,ungrade);
+
+
+// Aufagbe wurde mit Hilfe von Salem gelöst .
+
+
+
+
+     
+ 
+
+
+
