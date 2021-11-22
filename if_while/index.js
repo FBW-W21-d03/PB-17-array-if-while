d8abc762db2c98ea45c4e88730447abf7ce89ec2
sprotte <metatron1972@live.de>
2021-11-22 14:49:46 +0100

    sprotte

---
+++ if_while/index.js
@@ -0,0 +1,21 @@
+
+let grade = [];
+let ungrade = [];
+let ausgabe = 2;
+
+/*
+let i;
+for (i = 0; i < 100; i++)
+if ((i % 2) == 0)
+console.log("grade"); 
+*/
+
+
+while ((grade.length<=100) && (ungrade.length<=100)) {
+    grade.push(ausgabe);
+    ungrade.push(ausgabe+1);
+    ausgabe+=2;
+    if((grade.length == 100) && (ungrade.length == 100)){
+        console.log(grade, ungrade);
+    }
+        }
