04ca3e0295d6364617be912e4480f72712af1def
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-22 13:47:36 +0100

    Orhan#S lösung

---
+++ array_if_while.js
@@ -0,0 +1,20 @@
+let arr1 = [];
+let arr2 = [];
+
+
+while (arr1 <= 100) {
+    arr1++;
+
+    if (arr1 %2 == 0) {
+       console.log(arr1)
+    }    
+}
+
+
+while (arr2 <= 100) {
+    arr2++;
+    
+    if (arr2 %3 == 0) {
+       console.log(arr2)
+    }  
+}
